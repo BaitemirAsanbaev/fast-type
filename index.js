82247ded7c02bc66e95d4db55a355504ec0ae266
Baitemir <ferrari170604@gmail.com>
2021-03-18 23:23:36 +0600

    comparation logic

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let inp = document.querySelector('.inp');
 let words = document.querySelector('.words');
 let result = document.querySelector('result');
+let correntWord;
+let givenWord;
+words.innerHTML = givenWord;
+inp.addEventListener('input', () => {
+    correntWord = inp.value;
+})
+document.body.addEventListener('keydown', () => {
+    if (correntWord == givenWord){
 
-inp.addEventListener('input', (event) => {
-    words.innerText = this.value;
+    }
 })

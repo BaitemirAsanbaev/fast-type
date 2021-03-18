eec683f0fd6e43177d2f50bf77c37313036264a9
Baitemir <ferrari170604@gmail.com>
2021-03-18 23:36:13 +0600

    chacking words

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 let inp = document.querySelector('.inp');
-let words = document.querySelector('.words');
+let wordsDiv = document.querySelector('.words');
 let result = document.querySelector('result');
-let correntWord;
-let givenWord;
-words.innerHTML = givenWord;
+let words = ['level',
+        'shave',
+        'revoke',
+        'quit',
+        'sandwich',
+        'splurge',
+        'promise',
+        'problem',
+        'damn',
+        'bait',
+        'tendency',
+        'rape',
+        'edition',
+        'stable',
+        'federation',
+        'hard',
+        'choke',
+        'marine',
+        'pit',
+        'slam',]
+let currentWord;
+let givenWord
+words.forEach(element => {
+    wordsDiv.innerText = element;
+    givenWord = element;
+});
 inp.addEventListener('input', () => {
-    correntWord = inp.value;
+    currentWord = inp.value;
 })
-document.body.addEventListener('keydown', () => {
-    if (correntWord == givenWord){
-
+document.body.addEventListener('keydown', (event) => {
+    if(event.key == " "){
+        console.log(currentWord, givenWord)
+        if (currentWord == givenWord){
+            console.log("yes")
+            words.unshift(words.length -1)
+            words.forEach(element => {
+                wordsDiv.innerText = element;
+                givenWord = element;
+            });
+        }
     }
 })

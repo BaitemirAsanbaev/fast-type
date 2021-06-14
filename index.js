e6dd13a5fe845e4400ca98c1a4d133f9c334b3a8
Baitemir <ferrari170604@gmail.com>
2021-06-14 12:03:57 +0600

    formatted code

--- index.js
+++ index.js
@@ -4,77 +4,75 @@ let result = document.querySelector('.result');
 let time = document.querySelector('.time');
 let btn = document.querySelector('.start');
 let wordsCount = 0;
-let num = 60;
+let num;
 let interval;
 let isStarted = false;
 let words = ['level',
-        'shave',
-        'revoke',
-        'quit',
-        'sandwich',
-        'splurge',
-        'promise',
-        'problem',
-        'damn',
-        'bait',
-        'tendency',
-        'rape',
-        'edition',
-        'stable',
-        'federation',
-        'hard',
-        'choke',
-        'marine',
-        'pit',
-        'slam',]
+  'shave',
+  'revoke',
+  'quit',
+  'sandwich',
+  'splurge',
+  'promise',
+  'problem',
+  'damn',
+  'bait',
+  'tendency',
+  'rape',
+  'edition',
+  'stable',
+  'federation',
+  'hard',
+  'choke',
+  'marine',
+  'pit',
+  'slam',]
 let currentWord;
 let givenWord;
 words.forEach(element => {
-    wordsDiv.innerText = element;
-    givenWord = element;
+  wordsDiv.innerText = element;
+  givenWord = element;
 });
 
-
-
 inp.addEventListener('input', () => {
-    currentWord = inp.value;
+  currentWord = inp.value;
 
 })
 btn.addEventListener('click', () => {
-    isStarted = true;
-    num = 4;
-    setTimeout(() => {
-        result.innerText = wordsCount;
-    }, 4000);
-    interval = setInterval(() => {
-        num--;
-        time.innerText = num;
-        if(num <= 0){
-            clearInterval(interval)
-        }
-    }, 1000);
+  isStarted = true;
+  num = 4;
+  setTimeout(() => {
+    result.innerText = wordsCount;
+  }, 4000);
+  interval = setInterval(() => {
+    num--;
+    time.innerText = num;
+    if (num <= 0) {
+      clearInterval(interval)
+    }
+  }, 1000);
 
 })
 
 
 document.body.addEventListener('keydown', (event) => {
-    if(isStarted){
-        if(event.key == " "){
-            event.preventDefault();
-            if(currentWord!= givenWord){
-                wordsDiv.style.color = "red"
-            }
-            if (currentWord == givenWord){
-                wordsDiv.style.color = "#000"
-                wordsCount++;
-                words.pop();
-                words.forEach(element => {
-                    wordsDiv.innerText = element;
-                    givenWord = element;
-                });
-                inp.value = '';
-            }
-    
-        }
+  if (isStarted) {
+    if (event.key == " ") {
+      event.preventDefault();
+      if (currentWord != givenWord) {
+        wordsDiv.style.color = "red"
+      }
+      if (currentWord == givenWord) {
+        wordsDiv.style.color = "#000"
+        wordsCount++;
+        words.pop();
+        words.forEach(element => {
+          wordsDiv.innerText = element;
+          givenWord = element;
+        });
+        inp.value = '';
+      }
+
     }
+  }
 })
